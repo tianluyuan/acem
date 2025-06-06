70f52c005563b66afd831629b09688c8ebf9bcbb
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2025-06-05 19:17:34 -0500

    coolio

--- DataGen/DataGen_executable.sh
+++ DataGen/DataGen_executable.sh
@@ -4,10 +4,13 @@ cd $(dirname $0)
 echo $PWD
 hostname
 printenv
+uname -a
+lscpu
 
-python3 -m venv --copies v
-source v/bin/activate
-pip3 install -U numpy scipy pandas
+vtmp=`mktemp -d`
+python3 -m venv --copies $vtmp
+source $vtmp/bin/activate || export PYTHONUSERBASE=$vtmp
+pip3 install -U numpy scipy pandas -v
 
 num_runs=1
 seed_num=$(($1 * $num_runs))
@@ -17,17 +20,16 @@ echo "saving to " ${dataoutputdir}
 mkdir -p ${dataoutputdir}/job$1inps
 mkdir -p ${dataoutputdir}/job$1txts
 
-python gen_inp.py -s $num_runs -ss $seed_num -i inp_gen.csv -o ${dataoutputdir}/job$1inps -l True
+python3 gen_inp.py -s $num_runs -ss $seed_num -i inp_gen.csv -o ${dataoutputdir}/job$1inps -l True
 for file in ${dataoutputdir}/job$1inps/*.inp; do
     ./inp2txt.sh $file ${dataoutputdir}/job$1txts 1
 done
 rm ${dataoutputdir}/job$1txts/input.txt
 rm -r ${dataoutputdir}/job$1inps
 for file in ${dataoutputdir}/job$1txts/*.txt; do
-    python add_to_csv.py -txt $file -csv ${dataoutputdir}/job$1_`basename ${file%_[0-9]*_[0-9]*\.txt}`.csv
+    python3 add_to_csv.py -txt $file -csv ${dataoutputdir}/job$1_`basename ${file%_[0-9]*_[0-9]*\.txt}`.csv
 	rm $file
 done
 rm -r ${dataoutputdir}/job$1txts
-rm -r v
 
 tar -cvzf job$1_`basename ${dataoutputdir}`.tar.gz `basename ${dataoutputdir}` # easy way to transfer output back with htcondor
