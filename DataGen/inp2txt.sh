14b3dd12b2a0b0c0c891a278db9075c9e865c19b
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2025-07-08 21:19:19 -0500

    use flukadpmcerw

--- DataGen/inp2txt.sh
+++ DataGen/inp2txt.sh
@@ -21,7 +21,7 @@ num_runs="$3"
 og_dir=$(pwd) # save original directory
 cp $file_path $out_dir #copy .inp file into directory
 cd $out_dir # move to output directory to run fluka in
-${FLUPRO}/flutil/rfluka -e ${FLUPRO}/flukadpm3 -M$num_runs $F
+${FLUPRO}/flutil/rfluka -e ${FLUPRO}/flukadpm3cerw -M$num_runs $F
 rm ran${F}*
 for ((i = 1; i <= num_runs; i++)); do
     f_i=$(printf "%03d" $i) #formatted index (three digits)
