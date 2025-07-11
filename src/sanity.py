d10088bfee4fa1658fe42dd104b08f33940e00a1
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2025-07-10 20:45:22 -0500

    more sanity

--- src/sanity.py
+++ src/sanity.py
@@ -3,14 +3,15 @@ from matplotlib import pyplot as plt
 import FitSpline
 from scipy import stats
 plt.style.use('present')
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
 
 lrad = 0.358/0.9216 * 100 # cm
 estr = '1.00000E3'
 ppart = 'ELECTRON'
 npart = 3
 
-prop_cycle = plt.rcParams['axes.prop_cycle']
-colors = prop_cycle.by_key()['color']
+slt = np.s_[:, 501]
 
 a = np.loadtxt(f'DataOutputs_{ppart}/{ppart}_{estr}.csv', delimiter=',')
 dfs = FitSpline.load_ian(ppart, f'DataOutputs_{ppart}')
@@ -42,4 +43,26 @@ for _ in range(min(npart, len(x))):
 plt.ylabel('dl/dx')
 plt.xlabel('[cm]')
 plt.xlim(0, 2000)
+plt.show()
+
+# check ltot vs out.v1 (unweighted track length)
+b = np.loadtxt(f'out.v1/DataOutputs_{ppart}/{ppart}_{estr}.csv', delimiter=',')
+bins = np.linspace(min(a[slt].min(), b[slt].min()*1e7),
+                   max(a[slt].max(), b[slt].max()*1e7),
+                   100)
+plt.clf()
+plt.hist(a[slt], bins=bins, density=True, histtype='step')
+plt.hist(b[slt]*1e7, bins=bins, density=True, histtype='step')
+plt.xlabel('Total track length [cm]')
+plt.ylabel('density')
+plt.legend(['xy +/- 15m, density=0.9216', 'raw, xy +/- 5m, density=0.917'])
+plt.yscale('log')
+plt.show()
+
+# check saved dl/dx for random 3 vs bottom-5%-ltot 3 events
+plt.clf()
+al = a[a[slt] < np.quantile(a[slt], 0.05)]
+[plt.plot(range(500), a[_,:500], label=f'{a[_, 501]}') for _ in range(3)];
+[plt.plot(range(500), al[_,:500], linestyle='--', label=f'{al[_, 501]}') for _ in range(3)]
+plt.legend()
 plt.show()
