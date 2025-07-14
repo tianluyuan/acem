1ec98cfe7d9c77f7872da7d0079544c15ee03282
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2025-07-14 14:59:31 -0500

    use polynomial for mean, which is more important. tweaks here and there.

--- src/sanity.py
+++ src/sanity.py
@@ -8,14 +8,13 @@ colors = prop_cycle.by_key()['color']
 
 lrad = 0.358/0.9216 * 100 # cm
 estr = '1.00000E3'
-ppart = 'ELECTRON'
+ppart = 'PROTON'
 npart = 3
 
 slt = np.s_[:, 501]
-
-a = np.loadtxt(f'DataOutputs_{ppart}/{ppart}_{estr}.csv', delimiter=',')
-dfs = FitSpline.load_ian(ppart, f'DataOutputs_{ppart}')
-df = dfs[estr]
+fpath = f'DataOutputs_{ppart}/{ppart}_{estr}.csv'
+a = np.loadtxt(fpath, delimiter=',')
+df = FitSpline.load_csv(fpath)
 
 xdf = df[df['gammaA'] < 1.0001]
 x = a[df['gammaA'] < 1.0001]
@@ -26,11 +25,12 @@ for _ in range(npart):
     _nbins = int(_row['Zbins'])
     xs = np.arange(_nbins) * _row['Zwidth']
     ltot = _row['ltot']
-    plt.plot(xs, a[_, :_nbins], color=colors[_])
+    plt.plot(xs, a[_, :_nbins], color=colors[_], label=f'{ltot}')
     plt.plot(xs, ltot*stats.gamma.pdf(xs, _row['gammaA'], scale=lrad/_row['gammaB']), color=colors[_], linestyle='--')
 plt.ylabel('dl/dx')
 plt.xlabel('[cm]')
 plt.xlim(0, 2000)
+plt.legend()
 plt.show()
 
 for _ in range(min(npart, len(x))):
@@ -62,7 +62,7 @@ plt.show()
 # check saved dl/dx for random 3 vs bottom-5%-ltot 3 events
 plt.clf()
 al = a[a[slt] < np.quantile(a[slt], 0.05)]
-[plt.plot(range(500), a[_,:500], label=f'{a[_, 501]}') for _ in range(3)];
-[plt.plot(range(500), al[_,:500], linestyle='--', label=f'{al[_, 501]}') for _ in range(3)]
+[plt.plot(range(500), a[_,:500], label=f'{a[_, 501]}', c=colors[_]) for _ in range(3)];
+[plt.plot(range(500), al[_,:500], linestyle='--', label=f'{al[_, 501]}', c=colors[_]) for _ in range(3)]
 plt.legend()
 plt.show()
