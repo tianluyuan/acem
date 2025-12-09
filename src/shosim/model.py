ff5f246999df8276e6c07966a48cca3874f4b85b
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2025-12-09 08:37:38 -0600

    tighten tolerances

--- src/shosim/model.py
+++ src/shosim/model.py
@@ -345,8 +345,8 @@ class Parametrization1D(ModelBase):
         ...     for en in np.linspace(1, 6, 10):
         ...         sampa = a.THETAS[pdg].sample(en, 100, random_state=rng)
         ...         sampb = a.THETAS[pdg]._legacy_sample(en, 200, random_state=rng)
-        ...         assert stats.ks_2samp(sampa[:,0], sampb[:,0]).pvalue>0.003
-        ...         assert stats.ks_2samp(sampa[:,1], sampb[:,1]).pvalue>0.003
+        ...         assert stats.ks_2samp(sampa[:,0], sampb[:,0]).pvalue>0.01
+        ...         assert stats.ks_2samp(sampa[:,1], sampb[:,1]).pvalue>0.005
         """
         _size = 1 if size is None else size
         ltots = self.ltot_dist(pdg, energy).rvs(_size, random_state=self._rng)
